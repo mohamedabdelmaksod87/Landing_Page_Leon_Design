dd66ba3a643fc27cf3f01248fe74269faf22242d
mohamedabdelmaksod87 <87373024+mohamedabdelmaksod87@users.noreply.github.com>
2021-12-09 17:14:33 +0200

    design upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* When the user clicks on menu icon, toggle between hiding and showing the dropdown content */
+function showMenu() {
+  document.querySelector(".header .links ul").classList.toggle("show");
+  document.querySelector(".header .links .icon").classList.toggle("hide");
+  document.querySelector(".header .links .close-icon").classList.toggle("show");
+}
+
+//Scroll to top
+let span = document.querySelector(".up");
+
+window.onscroll = () => {
+  this.scrollY >= 800
+    ? span.classList.add("view")
+    : span.classList.remove("view");
+};
+
+span.onclick = () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+};
