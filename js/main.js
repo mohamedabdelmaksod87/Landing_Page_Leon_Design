d61d04dd389e62af3b148d0bb07eeca3009ab564
mohamedabdelmaksod87 <87373024+mohamedabdelmaksod87@users.noreply.github.com>
2021-12-12 11:32:04 +0200

    updated dropdown menu smooth animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /* When the user clicks on menu icon, toggle between hiding and showing the dropdown content */
 function showMenu() {
-  document.querySelector(".header ul").classList.toggle("show");
+  let panel = document.querySelector(".header ul");
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
   document.querySelector(".header .links .menu-icon").classList.toggle("hide");
   document.querySelector(".header .links .close-icon").classList.toggle("show");
 }
